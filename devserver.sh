e07241f0670021847a2e9204e6bb91ef3e1c562c
asiangoldfish <duongkhai@gmail.com>
2024-03-24 11:43:52 +0100

    devserver.sh crashes whenever a non-zero result occurs

--- devserver.sh
+++ devserver.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
-command -v bundle &> /dev/null || { echo "bundle is not installed or Jekyll is not correctly setup"; exit 1 }
+set -e
+
+command -v bundle &> /dev/null || { echo "bundle is not installed or Jekyll is not correctly setup"; exit 1; }
 
 bundle exec jekyll serve    --livereload \
                             --livereload-port 8080 \
